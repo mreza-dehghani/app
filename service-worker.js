e52929952b28b4f4361f61233072a2c54e86d0a6
mohammadreza <mrezavlb@gmail.com>
2020-09-09 21:13:49 +0430

    commited

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c1ae3e9a7ded8ab19cd48e6a9cfa780.js"
+  "/precache-manifest.7998981622e2ae829ae4dab4e1dade8f.js"
 );
 
 self.addEventListener('message', (event) => {
